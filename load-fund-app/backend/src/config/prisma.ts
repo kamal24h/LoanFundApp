c26f284cb8aa72e705971e5ea2cfe411020503e0
Kamal Baastani <kbastan@hotmail.com>
2025-12-17 17:12:05 +0330

    ++

--- load-fund-app/backend/src/config/prisma.ts
+++ load-fund-app/backend/src/config/prisma.ts
@@ -1,5 +1,10 @@
-import { PrismaClient } from "@prisma/client";
+//import { PrismaClient } from "@prisma/client";
+// const prisma = new PrismaClient();
 
-const prisma = new PrismaClient();
+import { PrismaPg } from '@prisma/adapter-pg'
+import { PrismaClient } from '@prisma/client'
+
+const adapter = new PrismaPg({ connectionString: process.env.DATABASE_URL })
+const prisma = new PrismaClient({ adapter })
 
 export default prisma;
